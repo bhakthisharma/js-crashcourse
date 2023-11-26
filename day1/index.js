8f31e7208d18b64353ba233106431f5a918f91ed
Bhakthishri S Sharma I <shribhakthi29@gmail.com>
2023-11-26 22:28:11 +0530

    day 1

---
+++ day1/index.js
@@ -0,0 +1,12 @@
+let score = 50;
+score = 20;
+console.log(score);
+let name = "Dubu";
+let age = 20;
+console.log("Data type of variable name is", typeof name);
+console.log("Data type of variable age is", typeof age);
+if (typeof name == "string" || typeof age == "string") {
+  console.log("Data type of variable name is String");
+}
+console.log(true && true);
+console.log(5 > 3 && 6 > 4);
