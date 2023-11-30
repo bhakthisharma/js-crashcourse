488d60bcbece7bafaa9ce028cb819255ad5f4e60
Bhakthishri S Sharma I <shribhakthi29@gmail.com>
2023-11-30 21:45:50 +0530

    Task1 of Day1&2

---
+++ task1-day1&2/index.js
@@ -0,0 +1,16 @@
+let data = [
+  "Orange",
+  29,
+  21,
+  "Dubu",
+  2129,
+  true,
+  null,
+  undefined,
+  "Dumpa",
+  "Dumpi",
+];
+
+for (let i = 0; i < data.length; i++) {
+  console.log("Array element:" + data[i] + " Type is:" + typeof data[i]);
+}
